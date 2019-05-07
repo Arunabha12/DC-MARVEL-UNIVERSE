04d742d8641abf69f3942181ceaaf41de9fd2e10
Arunabha12 <arunabhabanerjee2@gmail.com>
2019-05-07 23:51:47 +0530

    Updates

--- precache-manifest.48fdb84eef53fa1010425f94474f8647.js
+++ precache-manifest.48fdb84eef53fa1010425f94474f8647.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ca79358413fcb648986fa50af02d049a",
+    "revision": "9240aee69cbea6cc7a22271ff178f70f",
     "url": "/DC-MARVEL-UNIVERSE/index.html"
   },
   {
-    "revision": "fd65c6d93bff7b0b24dc",
-    "url": "/DC-MARVEL-UNIVERSE/static/css/main.273f7a94.chunk.css"
+    "revision": "583aaa4022a8b4d02cd3",
+    "url": "/DC-MARVEL-UNIVERSE/static/css/main.2a4a8ef8.chunk.css"
   },
   {
     "revision": "ebe9b4964156c3872d40",
     "url": "/DC-MARVEL-UNIVERSE/static/js/2.0de03e2a.chunk.js"
   },
   {
-    "revision": "fd65c6d93bff7b0b24dc",
-    "url": "/DC-MARVEL-UNIVERSE/static/js/main.caf168c2.chunk.js"
+    "revision": "583aaa4022a8b4d02cd3",
+    "url": "/DC-MARVEL-UNIVERSE/static/js/main.1b4aa9fd.chunk.js"
   },
   {
     "revision": "ef97d82b23b089e6922f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DC-MARVEL-UNIVERSE/precache-manifest.7be2c8e6c26cd1ffdf780bc590853e9e.js"
+  "/DC-MARVEL-UNIVERSE/precache-manifest.48fdb84eef53fa1010425f94474f8647.js"
 );
 
 self.addEventListener('message', (event) => {
