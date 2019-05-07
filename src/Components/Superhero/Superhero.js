9438fb8bdcb047ee7e4b4046d530f32b074e0bfe
Arunabha12 <arunabhabanerjee2@gmail.com>
2019-05-07 23:50:34 +0530

    files changed

--- src/Components/Superhero/Superhero.js
+++ src/Components/Superhero/Superhero.js
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import axios from '../../axios';
 import Hero from '../Hero/Hero';
 import classes from './Superhero.module.css';
@@ -7,45 +7,51 @@ import classes from './Superhero.module.css';
 class Superhero extends Component {
     state = {
         heroes: [],
-        query: ''
+        query: '',
+        error: null
     }
-    getresult=()=>{
-        
+    getresult = () => {
         axios.get(`/${this.state.query}`)
-        .then(response => {
-            this.setState({heroes: response.data.results});
-            console.log(response)
+            .then(response => {
+                if (response.data.error) {
+                    this.setState({ error: response.data.error });
+                }
+                else {
+                    this.setState({ heroes: response.data.results,error: null });
+                }//console.log(response)
+
             })
-        .catch(error =>{
-                console.log(error)
+            .catch(error => {
+                console.log(error);
             });
     }
 
-    inputHandler=(event)=>{
-        this.setState({query:event.target.value},()=>{
+    inputHandler = (event) => {
+        this.setState({ query: event.target.value }, () => {
 
         })
     }
 
-    submitHandler=()=>{
+    submitHandler = () => {
         this.getresult();
+        console.log(this.state.heroes.length)
     }
 
+
     render() {
-        const heroes = this.state.heroes.map(hero =>{
-            return <Hero
-                data={hero}
-                key={hero.id}
-                />
-        });
-        return(
-            <div className={classes.Background} style={{minHeight:`${window.innerHeight}px`}}>
+        return (
+            <div className={classes.Background}>
                 <div className={classes.SearchBar}>
-                    <input className={classes.Input} type='text' placeholder='Search Heroes and Villains' value={this.state.query} onChange={this.inputHandler}/>
+                    <input className={classes.Input} type='text' placeholder='Search Heroes and Villains' value={this.state.query} onChange={this.inputHandler} />
                     <button className={classes.Button} onClick={this.submitHandler} type="submit">Search</button>
                 </div>
                 <div>
-                    {heroes}
+                    {this.state.error ? <p>{this.state.error}</p> : this.state.heroes.map(hero => {
+                        return <Hero
+                            data={hero}
+                            key={hero.id}
+                        />
+                    })}
                 </div>
             </div>
         );
